e6161289a0f6275a069b365b108a8207a7862a64
SayedZubair <sayedzubairhashimi2020>
2022-09-28 17:48:44 -0700

    second commit

--- src/finalProject/FinalClass.java
+++ src/finalProject/FinalClass.java
@@ -4,7 +4,8 @@ public class FinalClass {
 
 	public static void main(String[] args) {
 	System.out.println("This is the code for the Mercedez");
-
+/// we need to run the project each time 
+	// we need to right click on the project to get the address its wrong to get the adress from the class
 	}
 
 }
